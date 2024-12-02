add11e38b5ae5e3b8c1907be04690f063a5dbcac
Akiva Cohen <Akivaecohen@gmail.com>
2024-12-02 18:37:56 -0500

    1.3.0 added toggle switch

--- content.js
+++ content.js
@@ -1,34 +1,42 @@
-
-const isThisShipyard = () => window.location.href.includes("shipyard");
-const pointsToVotes = (points) => Math.round(((points - 0.5) * 10) / 24.5);
-
-/**@type {HTMLHeadingElement} */
-let shippedShipsHeader = null;
-
-let lastLoadState = false;
-
-let loaderInterval = setInterval(() => {
-    if (!isThisShipyard()) return;
-
-    shippedShipsHeader = [...document.querySelectorAll("h2").values()]?.filter(el => el?.textContent === "Shipped Ships")?.[0];
-    if (shippedShipsHeader) {
-        if (!lastLoadState) {
-            lastLoadState = true;
-            runCode();
-        } else if ([...document?.querySelectorAll('[id^="shipped-ship-"]:has(button)')].map(el => el?.querySelector(".flex-grow > div").children.length <= 2).includes(true)) {
+//checks if button checked
+new Promise((resolve) => {
+    chrome.local.storage.get(["on"], function (output) {
+        x = output.on;
+        if (x || x === undefined) {
             runCode();
         }
-    } else {
-        shippedShipsHeader = null;
-        lastLoadState = false;
-    }
-}, 1000);
+    });
+});
 
-const HTML_CONTENT = (averagePerHour, averagePerProject) => `
-<div id="hs-ext-features" class="rounded-lg bg-card text-card-foreground shadow-sm bg-blend-color-burn flex flex-col sm:gap-2 sm:flex-row items-start sm:items-center p-4 hover:bg-gray-100 transition-colors duration-200" style="background-size: 10rem 100%;background-repeat: repeat-x;background-color: rgba(255, 255, 255, 0.94);">
+function runCode() {
+    const isThisShipyard = () => window.location.href.includes("shipyard");
+    const pointsToVotes = (points) => Math.round(((points - 0.5) * 10) / 24.5);
+    /**@type {HTMLHeadingElement} */
+    let shippedShipsHeader = null;
+    
+    let lastLoadState = false;
+
+    let loaderInterval = setInterval(() => {
+        if (!isThisShipyard()) return;
+
+        shippedShipsHeader = [...document.querySelectorAll("h2").values()]?.filter(el => el?.textContent === "Shipped Ships")?.[0];
+        if (shippedShipsHeader) {
+            if (!lastLoadState) {
+                lastLoadState = true;
+                runCode();
+            } else if ([...document?.querySelectorAll('[id^="shipped-ship-"]:has(button)')].map(el => el?.querySelector(".flex-grow > div").children.length <= 2).includes(true)) {
+                runCode();
+            }
+        } else {
+            shippedShipsHeader = null;
+            lastLoadState = false;
+        }
+    }, 1000);
+    const HTML_CONTENT = (averagePerHour, averagePerProject) => `
+    <div id="hs-ext-features" class="rounded-lg bg-card text-card-foreground shadow-sm bg-blend-color-burn flex flex-col sm:gap-2 sm:flex-row items-start sm:items-center p-4 hover:bg-gray-100 transition-colors duration-200" style="background-size: 10rem 100%;background-repeat: repeat-x;background-color: rgba(255, 255, 255, 0.94);">
     <p id="hs-ext-title">HighSeas Doubloons Extension</p>
     <div class="flex flex-wrap items-start gap-3 text-sm items-center">
-        <span class="mr-2 text-xl font-semibold">Average: </span>
+    <span class="mr-2 text-xl font-semibold">Average: </span>
         <span class="inline-flex items-center gap-1 rounded-full px-2 border text-sm leading-none text-green-600 bg-green-50 border-green-500/10 " style="vertical-align: middle;">
             <img alt="doubloons" loading="lazy" width="16" height="20" decoding="async" data-nimg="1" src="/_next/static/media/doubloon.fd63888b.svg" style="color: transparent;">
             <span class="inline-block py-1">${averagePerHour?.toFixed(2)} / hour</span>
@@ -61,7 +69,6 @@ const HTML_CONTENT = (averagePerHour, averagePerProject) => `
     }
 </style>
 `;
-
 const SVG_VOTES_ICON = `<svg fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" preserveAspectRatio="xMidYMid meet" fill="currentColor" width="18" height="18" style="display: inline-block; vertical-align: middle;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm64 192c17.7 0 32 14.3 32 32l0 96c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-96c0-17.7 14.3-32 32-32zm64-64c0-17.7 14.3-32 32-32s32 14.3 32 32l0 192c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-192zM320 288c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-32c0-17.7 14.3-32 32-32z"/></svg>`;
 
 function getAverages() {
@@ -144,4 +151,5 @@ const HTML_SCRIPT = () => {
 
 function runCode() {
     HTML_SCRIPT();
+}
 }
---
+++ stor.js
@@ -0,0 +1,22 @@
+//create button
+const check = document.getElementById("onOff");
+//set switch at open
+new Promise(() => {
+    chrome.storage.local.get(["on"], function (output) {
+        x = output.on;
+        if (x === undefined) {
+            check.checked = true;
+        } else {
+            check.checked = x;
+        }
+    });
+});
+
+//listen for switch flick
+check.addEventListener("change", function() {
+    if (check.checked) {
+        chrome.local.storage.set({"on":true});
+    } else {
+        chrome.local.storage.set({"on":false});
+    }
+});

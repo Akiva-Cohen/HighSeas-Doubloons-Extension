330fd67ebab9dedcdfd20de550a624445f98ffac
Akiva Cohen <Akivaecohen@gmail.com>
2024-12-03 16:31:49 -0500

    v1.6.0 wonderdome stop popup

--- Wonder.js
+++ Wonder.js
@@ -1,17 +1,30 @@
+console.log("active");  
 y = new Promise((resolve) => {
     chrome.storage.local.get(["on"], function (output) {
         x = output.on;
         if (x === undefined) {
             resolve(true);
         }
-        resolve(x);
+        if (x) {
+            resolve(true);
+        } else {
+            resolve(false);
+        }
     });
 });
 if (y) {
-    addCode;
+    addCode();
 }
-function addCode() {
-    addEventListener("beforeunload", (event) => {
-        event.preventDefault();
-    })
+async function addCode() {
+    console.log("adding");
+    const script = document.createElement("script");
+    script.src = chrome.runtime.getURL('Stop.js');
+    //script.onload = function() {this.remove();};
+    document.body.appendChild(script);
+    console.log(script);
+    /*let [tab] = await chrome.tabs.query({active:true,currentWindow:true});
+    await chrome.scripting.executeScript({
+        target: {tabId : tab.id},
+        files : ["Stop.js"]
+    })*/
 }
